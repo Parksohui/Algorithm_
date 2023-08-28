aa47904a5e864419095d9964d92288d9ab03709e
Parksohui <psh990525@naver.com>
2023-08-28 19:35:36 +0900

    2257 화학식량

---
+++ Baekjoon/_2257_.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class _2257_ { // 화학식량
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = bf.readLine();
+
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < str.length(); i++) {
+			Character ch = str.charAt(i);
+			if (ch == 'C') {
+				stack.add(12);
+			} else if (ch == 'O') {
+				stack.add(16);
+			} else if (ch == 'H') {
+				stack.add(1);
+			} else if (Character.isDigit(ch)) {
+				int num = stack.pop();
+				stack.add(num * (ch - '0'));
+			} else if (ch == '(') {
+				stack.add(-1);
+			} else if (ch == ')') {
+				int num = 0;
+				while (stack.peek() != -1) {
+					num += stack.pop();
+				}
+				stack.pop();
+				stack.add(num);
+			}
+		}
+
+		int result = 0;
+		while (!stack.isEmpty()) {
+			result += stack.pop();
+		}
+		
+		System.out.println(result);
+	}
+}
