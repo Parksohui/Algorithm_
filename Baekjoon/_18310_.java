66bb80251bc296c40d94351da21eb13f398d1752
Parksohui <psh990525@naver.com>
2023-09-26 10:57:55 +0900

    18310 안테나

---
+++ Baekjoon/_18310_.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _18310_ { // 안테나
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(bf.readLine());
+
+		st = new StringTokenizer(bf.readLine());
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		int idx = 0;
+		if (n % 2 == 0)
+			idx = n / 2 - 1;
+		else
+			idx = n / 2;
+		
+		System.out.println(arr[idx]);
+	}
+}
