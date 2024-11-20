32d7881da0eecfbc760d2022ec1cb08c96f37759
Parksohui <psh990525@naver.com>
2024-11-20 13:43:29 +0900

    6004 The Chivalrous Cow

---
+++ Baekjoon/_6004_.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class _6004_ { // The Chivalrous Cow
+	static int dx[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+	static int dy[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
+	static boolean arr[][];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+
+		arr = new boolean[y][x];
+		int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
+		for (int i = 0; i < y; i++) {
+			String str = bf.readLine();
+			for (int j = 0; j < x; j++) {
+				if (str.charAt(j) == '*') {
+					arr[i][j] = true;
+				} else if (str.charAt(j) == 'K') {
+					x1 = i;
+					y1 = j;
+				} else if (str.charAt(j) == 'H') {
+					x2 = i;
+					y2 = j;
+				}
+			}
+		}
+		System.out.println(bfs(x1, y1, x2, y2));
+	}
+
+	private static int bfs(int x1, int y1, int x2, int y2) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] { x1, y1, 0 });
+
+		arr[x1][y1] = true;
+
+		while (!queue.isEmpty()) {
+			int temp[] = queue.poll();
+			for (int i = 0; i < 8; i++) {
+				int x = temp[0] + dx[i];
+				int y = temp[1] + dy[i];
+				if (x >= 0 && x < arr.length && y >= 0 && y < arr[0].length && !arr[x][y]) {
+					if (x == x2 && y == y2) {
+						return temp[2] + 1;
+					}
+					arr[x][y] = true;
+					queue.add(new int[] { x, y, temp[2] + 1 });
+				}
+			}
+		}
+		return -1;
+	}
+}
